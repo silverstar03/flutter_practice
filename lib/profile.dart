d9485caed550bb85144864ef243313184bfb08d8
silverstar03 <eunbyoul0421@naver.com>
2021-04-04 23:37:00 +0900

    firebase storage 이미지 업로드 연습

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,184 +1,56 @@
-// import 'dart:io';
-//
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:image_picker/image_picker.dart';
-// import 'package:firebase_storage/firebase_storage.dart';
-//
-// CloudStorageDemoState pageState;
-//
-// class CloudStorageDemo extends StatefulWidget {
-//   @override
-//   CloudStorageDemoState createState() {
-//     pageState = CloudStorageDemoState();
-//     return pageState;
-//   }
-// }
-//
-// class CloudStorageDemoState extends State<CloudStorageDemo> {
-//   File _image;
-//   FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-//   FirebaseUser _user;
-//   FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
-//   String _profileImageURL = "";
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _prepareService();
-//   }
-//
-//   void _prepareService() async {
-//     _user = await _firebaseAuth.currentUser();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text("Cloud Storage Demo")),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             // 업로드할 이미지를 출력할 CircleAvatar
-//             CircleAvatar(
-//               backgroundImage:
-//               (_image != null) ? FileImage(_image) : NetworkImage(""),
-//               radius: 30,
-//             ),
-//             // 업로드할 이미지를 선택할 이미지 피커 호출 버튼
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: <Widget>[
-//                 RaisedButton(
-//                   child: Text("Gallery"),
-//                   onPressed: () {
-//                     _uploadImageToStorage(ImageSource.gallery);
-//                   },
-//                 ),
-//                 RaisedButton(
-//                   child: Text("Camera"),
-//                   onPressed: () {
-//                     _uploadImageToStorage(ImageSource.camera);
-//                   },
-//                 )
-//               ],
-//             ),
-//             Divider(
-//               color: Colors.grey,
-//             ),
-//             // 업로드 된 이미지를 출력할 CircleAvatar
-//             CircleAvatar(
-//               backgroundImage: NetworkImage(_profileImageURL),
-//               radius: 30,
-//             ),
-//             // 업로드 된 이미지의 URL
-//             Padding(
-//               padding: const EdgeInsets.all(8.0),
-//               child: Text(_profileImageURL),
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//
-//   void _uploadImageToStorage(ImageSource source) async {
-//     File image = await ImagePicker.pickImage(source: source);
-//
-//     if (image == null) return;
-//     setState(() {
-//       _image = image;
-//     });
-//
-//     // 프로필 사진을 업로드할 경로와 파일명을 정의. 사용자의 uid를 이용하여 파일명의 중복 가능성 제거
-//     StorageReference storageReference =
-//     _firebaseStorage.ref().child("profile/${_user.uid}");
-//
-//     // 파일 업로드
-//     StorageUploadTask storageUploadTask = storageReference.putFile(_image);
-//
-//     // 파일 업로드 완료까지 대기
-//     await storageUploadTask.onComplete;
-//
-//     // 업로드한 사진의 URL 획득
-//     String downloadURL = await storageReference.getDownloadURL();
-//
-//     // 업로드된 사진의 URL을 페이지에 반영
-//     setState(() {
-//       _profileImageURL = downloadURL;
-//     });
-//   }
-// }
-
-
-
-
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-void main() {
-  runApp(CloudStorageDemo());
-}
+void main() => runApp(MyApp());
 
-
-CloudStorageDemoState pageState;
-
-class CloudStorageDemo extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  CloudStorageDemoState createState() {
-    pageState = CloudStorageDemoState();
-    return pageState;
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
   }
 }
 
-class CloudStorageDemoState extends State<CloudStorageDemo> {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   File _image;
+  final picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Cloud Storage Demo")),
+      appBar: AppBar(
+        title: Text('Image Picker Example'),
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              backgroundImage:
-              (_image != null) ? FileImage(_image) : NetworkImage(""),
-              radius: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                RaisedButton(
-                  child: Text("Gallery"),
-                  onPressed: () {
-                    _uploadImageToStorage(ImageSource.gallery);
-                  },
-                ),
-                RaisedButton(
-                  child: Text("Camera"),
-                  onPressed: () {
-                    _uploadImageToStorage(ImageSource.camera);
-                  },
-                )
-              ],
-            )
-          ],
-        ),
+        child: _image == null
+            ? Text('No image selected.')
+            : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
       ),
     );
-  }
-
-  void _uploadImageToStorage(ImageSource source) async {
-    // File image = await ImagePicker.pickImage(source: source);
-    //
-    // if (image == null) return;
-    // setState(() {
-    //   _image = image;
-    // });
   }
 }
